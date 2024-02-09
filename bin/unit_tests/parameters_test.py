5ef7c94ebcdf2f7624c67e6afb98e63f5ef3d768
dr-joe-wirth <dr.joe.wirth@gmail.com>
2024-02-09 11:14:04 -0500

    revised test suite to handle changes to `Parameters` class

--- bin/unit_tests/parameters_test.py
+++ bin/unit_tests/parameters_test.py
@@ -1,16 +1,24 @@
 import os, pickle, sys, unittest
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 from bin.Parameters import Parameters, Log
 
 
 class ParametersTest(unittest.TestCase):
     # values for inputs
-    IG_FNS = ('itmp1.testfile', 'itmp2.testfile', 'itmp3.testfile')
-    OG_FNS = ('otmp4.testfile', 'otmp5.testfile', 'otmp6.testfile')
-    IG_PATTERN = "itmp*testfile"
-    OG_PATTERN = "otmp*testfile"
+    IG_FNS_GB = ('itmp1.gb', 'itmp2.gb', 'itmp3.gb')
+    IG_FNS_FA = ('itmp1.fa', 'itmp2.fa', 'itmp3.fa')
+    OG_FNS_GB = ('otmp1.gb', 'otmp2.gb', 'otmp3.gb')
+    OG_FNS_FA = ('otmp1.fa', 'otmp2.fa', 'otmp3.fa')
+    IG_PATTERN_GB = "itmp*gb"
+    IG_PATTERN_FA = "itmp*fa"
+    OG_PATTERN_GB = "otmp*gb"
+    OG_PATTERN_FA = "otmp*fa"
     OUT_FN = "result.testfile"
     BAD_SIZE = "64,160"
-    FORMAT = "genbank"
+    FORMAT_GB = "genbank"
+    FORMAT_FA = "fasta"
     PRIMER_LEN = "14,25"
     GC_RANGE = "42,64"
     TM_RANGE = "68,72"
@@ -18,8 +26,13 @@ class ParametersTest(unittest.TestCase):
     TM_DIFF = "4.2"
     THREADS = "8"
     DUMP_FN = "tmp.p"
-    VERSION = 'null'
+    VERSION = 'parameters_test'
     AUTHOR = 'null'
+    TEST_SEQ = SeqRecord(Seq('atcg'),
+                         id='id',
+                         name='name',
+                         description='description',
+                         annotations={'molecule_type':'DNA'})
     
     
     def setUp(self) -> None:
@@ -34,41 +47,70 @@ class ParametersTest(unittest.TestCase):
         
         # sys.argv for default values using short args
         self.basic1 = ['primerForge.py',
-                       '-i', ParametersTest.IG_PATTERN,
+                       '-i', ParametersTest.IG_PATTERN_GB,
                        '-o', ParametersTest.OUT_FN]
         
         # sys.argv for default values using long args
         self.basic2 = ['primerForge.py',
-                       '--ingroup', ParametersTest.IG_PATTERN,
+                       '--ingroup', ParametersTest.IG_PATTERN_GB,
                        '--out', ParametersTest.OUT_FN]
         
-        # sys.argv with custom values using short args
-        self.short  = ['primerForge.py',
-                       '-i', ParametersTest.IG_PATTERN,
-                       '-o', ParametersTest.OUT_FN,
-                       '-u', ParametersTest.OG_PATTERN,
-                       '-b', ParametersTest.BAD_SIZE,
-                       '-f', ParametersTest.FORMAT,
-                       '-p', ParametersTest.PRIMER_LEN,
-                       '-g', ParametersTest.GC_RANGE,
-                       '-t', ParametersTest.TM_RANGE,
-                       '-r', ParametersTest.PCR_SIZE,
-                       '-d', ParametersTest.TM_DIFF,
-                       '-n', ParametersTest.THREADS]
+        # sys.argv with custom values using short args for genbank file format
+        self.short1  = ['primerForge.py',
+                        '-i', ParametersTest.IG_PATTERN_GB,
+                        '-o', ParametersTest.OUT_FN,
+                        '-u', ParametersTest.OG_PATTERN_GB,
+                        '-b', ParametersTest.BAD_SIZE,
+                        '-f', ParametersTest.FORMAT_GB,
+                        '-p', ParametersTest.PRIMER_LEN,
+                        '-g', ParametersTest.GC_RANGE,
+                        '-t', ParametersTest.TM_RANGE,
+                        '-r', ParametersTest.PCR_SIZE,
+                        '-d', ParametersTest.TM_DIFF,
+                        '-n', ParametersTest.THREADS]
+        
+        # sys.argv with custom values using short args for fasta file format
+        self.short2  = ['primerForge.py',
+                        '-i', ParametersTest.IG_PATTERN_FA,
+                        '-o', ParametersTest.OUT_FN,
+                        '-u', ParametersTest.OG_PATTERN_FA,
+                        '-b', ParametersTest.BAD_SIZE,
+                        '-f', ParametersTest.FORMAT_FA,
+                        '-p', ParametersTest.PRIMER_LEN,
+                        '-g', ParametersTest.GC_RANGE,
+                        '-t', ParametersTest.TM_RANGE,
+                        '-r', ParametersTest.PCR_SIZE,
+                        '-d', ParametersTest.TM_DIFF,
+                        '-n', ParametersTest.THREADS]
+        
 
-        # sys.argv with custom values using long args
-        self.long   =  ['primerForge.py',
-                       '--ingroup', ParametersTest.IG_PATTERN,
-                       '--out', ParametersTest.OUT_FN,
-                       '--outgroup', ParametersTest.OG_PATTERN,
-                       '--bad_sizes', ParametersTest.BAD_SIZE,
-                       '--format', ParametersTest.FORMAT,
-                       '--primer_len', ParametersTest.PRIMER_LEN,
-                       '--gc_range', ParametersTest.GC_RANGE,
-                       '--tm_range', ParametersTest.TM_RANGE,
-                       '--pcr_prod', ParametersTest.PCR_SIZE,
-                       '--tm_diff', ParametersTest.TM_DIFF,
-                       '--num_threads', ParametersTest.THREADS]
+        # sys.argv with custom values using long args for genbank file format
+        self.long1   =  ['primerForge.py',
+                        '--ingroup', ParametersTest.IG_PATTERN_GB,
+                        '--out', ParametersTest.OUT_FN,
+                        '--outgroup', ParametersTest.OG_PATTERN_GB,
+                        '--bad_sizes', ParametersTest.BAD_SIZE,
+                        '--format', ParametersTest.FORMAT_GB,
+                        '--primer_len', ParametersTest.PRIMER_LEN,
+                        '--gc_range', ParametersTest.GC_RANGE,
+                        '--tm_range', ParametersTest.TM_RANGE,
+                        '--pcr_prod', ParametersTest.PCR_SIZE,
+                        '--tm_diff', ParametersTest.TM_DIFF,
+                        '--num_threads', ParametersTest.THREADS]
+        
+        # sys.argv with custom values using long args for genbank file format
+        self.long2   =  ['primerForge.py',
+                        '--ingroup', ParametersTest.IG_PATTERN_FA,
+                        '--out', ParametersTest.OUT_FN,
+                        '--outgroup', ParametersTest.OG_PATTERN_FA,
+                        '--bad_sizes', ParametersTest.BAD_SIZE,
+                        '--format', ParametersTest.FORMAT_FA,
+                        '--primer_len', ParametersTest.PRIMER_LEN,
+                        '--gc_range', ParametersTest.GC_RANGE,
+                        '--tm_range', ParametersTest.TM_RANGE,
+                        '--pcr_prod', ParametersTest.PCR_SIZE,
+                        '--tm_diff', ParametersTest.TM_DIFF,
+                        '--num_threads', ParametersTest.THREADS]
         
         # additional sys.argv to test
         self.help1  = ['primerForge.py', '-h']
@@ -77,8 +119,8 @@ class ParametersTest(unittest.TestCase):
         self.vers2  = ['primerForge.py', '--version']
         self.debug1 = self.basic1 + ['--debug']
         self.debug2 = self.basic2 + ['--debug']
-        self.debug3 = self.short + ['--debug']
-        self.debug4 = self.long + ['--debug']
+        self.debug3 = self.short1 + ['--debug']
+        self.debug4 = self.long1 + ['--debug']
     
     def tearDown(self) -> None:
         """tear downs after each test
@@ -93,14 +135,16 @@ class ParametersTest(unittest.TestCase):
     def _makeDummyFiles() -> None:
         """creates the dummy files
         """
-        for fn in ParametersTest.IG_FNS + ParametersTest.OG_FNS:
-            fh = open(fn, 'w')
-            fh.close()
-    
+        for fn in ParametersTest.IG_FNS_GB + ParametersTest.OG_FNS_GB:
+            SeqIO.write(ParametersTest.TEST_SEQ, fn, ParametersTest.FORMAT_GB)
+        
+        for fn in ParametersTest.IG_FNS_FA + ParametersTest.OG_FNS_FA:
+            SeqIO.write(ParametersTest.TEST_SEQ, fn, ParametersTest.FORMAT_FA)
+
     def _removeDummyFiles() -> None:
         """removes the dummy files
         """
-        for fn in ParametersTest.IG_FNS + ParametersTest.OG_FNS:
+        for fn in ParametersTest.IG_FNS_GB + ParametersTest.OG_FNS_GB + ParametersTest.IG_FNS_FA + ParametersTest.OG_FNS_FA:
             os.remove(fn)
         if os.path.exists(ParametersTest.DUMP_FN):
             os.remove(ParametersTest.DUMP_FN)
@@ -112,7 +156,7 @@ class ParametersTest(unittest.TestCase):
             params (Parameters): Parameters object
         """
         # make sure the ingroup files were correctly parsed
-        for fn in self.IG_FNS:
+        for fn in self.IG_FNS_GB:
             self.assertIn(fn, params.ingroupFns)
         
         # make sure the outfile is correct
@@ -134,20 +178,36 @@ class ParametersTest(unittest.TestCase):
         self.assertEqual(params.helpRequested, Parameters._DEF_HELP)
         self.assertEqual(params.debug, Parameters._DEF_DEBUG)
         self.assertEqual(params.disallowedLens, range(Parameters._DEF_MIN_PCR, Parameters._DEF_MAX_PCR+1))
+
+    def _checkGenomeFilesPresent(self, params:Parameters, frmt:str) -> None:
+        """checks if the genome files are present
+
+        Args:
+            params (Parameters): Parameters object
+            frmt (str): the format of the genome files
+        """
+        if frmt == ParametersTest.FORMAT_GB:
+            expectedIngroupFns  = ParametersTest.IG_FNS_GB
+            expectedOutgroupFns = ParametersTest.OG_FNS_GB
+        else:
+            expectedIngroupFns  = ParametersTest.IG_FNS_FA
+            expectedOutgroupFns = ParametersTest.OG_FNS_FA
+            
+        for fn in params.ingroupFns:
+            self.assertIn(fn, expectedIngroupFns)
+        
+        # check for all outgroup files
+        for fn in params.outgroupFns:
+            self.assertIn(fn, expectedOutgroupFns)
     
-    def _checkCustomValues(self, params:Parameters) -> None:
+    def _checkCustomValues(self, params:Parameters, frmt:str) -> None:
         """evaluates that params has the appropriate values when custom values are specified
 
         Args:
             params (Parameters): a Parameters object
         """
-        # check for all ingroup files
-        for fn in params.ingroupFns:
-            self.assertIn(fn, ParametersTest.IG_FNS)
-        
-        # check for all outgroup files
-        for fn in params.outgroupFns:
-            self.assertIn(fn, ParametersTest.OG_FNS)
+        # check for all genome files
+        self._checkGenomeFilesPresent(params, frmt)
         
         # parse values from the inputs
         minLen,maxLen = map(int, ParametersTest.PRIMER_LEN.split(','))
@@ -163,7 +223,7 @@ class ParametersTest(unittest.TestCase):
         
         # make sure the parameters are correct
         self.assertEqual(params.outFn, ParametersTest.OUT_FN)
-        self.assertEqual(params.format, ParametersTest.FORMAT)
+        self.assertEqual(params.format, frmt)
         self.assertEqual(params.minLen, minLen)
         self.assertEqual(params.maxLen, maxLen)
         self.assertEqual(params.minGc, minGc)
@@ -176,7 +236,7 @@ class ParametersTest(unittest.TestCase):
         self.assertEqual(params.numThreads, threads)
         self.assertEqual(params.disallowedLens, badSizes)
         self.assertEqual(params.helpRequested, Parameters._DEF_HELP)
-        self.assertEqual(params.debug, Parameters._DEF_DEBUG)    
+        self.assertEqual(params.debug, Parameters._DEF_DEBUG)
     
     def _dumpLoadTest(self, params:Parameters, obj) -> None:
         """evaluates if the dumpObj method is working
@@ -210,19 +270,33 @@ class ParametersTest(unittest.TestCase):
         params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
         self._checkDefaultValues(params)
 
-    def testC_parseShort(self) -> None:
-        """are args parsed with short flags and custom values
+    def testC_parseShort1(self) -> None:
+        """are args parsed with short flags and custom values for genbank files
         """
-        sys.argv = self.short
+        sys.argv = self.short1
         params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
-        self._checkCustomValues(params)
+        self._checkCustomValues(params, ParametersTest.FORMAT_GB)
     
-    def testD_parseLong(self) -> None:
-        """are args parsed with long flags and custom args
+    def testD_parseShort2(self) -> None:
+        """are args parsed with short flags and custom values for fasta files
         """
-        sys.argv = self.long
+        sys.argv = self.short2
         params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
-        self._checkCustomValues(params)
+        self._checkCustomValues(params, ParametersTest.FORMAT_FA)
+    
+    def testE_parseLong1(self) -> None:
+        """are args parsed with long flags and custom args for genbank files
+        """
+        sys.argv = self.long1
+        params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
+        self._checkCustomValues(params, ParametersTest.FORMAT_GB)
+    
+    def testF_parseLong2(self) -> None:
+        """are args parsed with long flags and custom args for genbank files
+        """
+        sys.argv = self.long2
+        params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
+        self._checkCustomValues(params, ParametersTest.FORMAT_FA)
     
     def testE_parseHelp(self) -> None:
         """is params.helpRequested True when help is requested
@@ -272,14 +346,14 @@ class ParametersTest(unittest.TestCase):
         params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
         self.assertTrue(params.debug)
         params.debug = False
-        self._checkCustomValues(params)
+        self._checkCustomValues(params, Parameters._DEF_FRMT)
         
         # check long flags with custom args
         sys.argv = self.debug4
         params = Parameters(ParametersTest.AUTHOR, ParametersTest.VERSION)
         self.assertTrue(params.debug)
         params.debug = False
-        self._checkCustomValues(params)
+        self._checkCustomValues(params, Parameters._DEF_FRMT)
     
     def testH_debug2(self) -> None:
         """is the logger working
