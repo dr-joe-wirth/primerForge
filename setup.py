a97894d80e923ca74f14a166395d7e75c86e2adc
dr-joe-wirth <dr.joe.wirth@gmail.com>
2024-04-22 16:01:32 -0400

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='primerforge',
-    version='1.0.1',
+    version='1.0.2',
     author='Joseph S. Wirth',
     packages=find_packages(),
     description='software to identify primers that can be used to distinguish genomes',
