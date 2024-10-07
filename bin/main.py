2c17a641128dbc91f68514401556ec9dd90c35ff
dr-joe-wirth <dr.joe.wirth@gmail.com>
2024-10-07 14:43:35 -0400

    increment version

--- bin/main.py
+++ bin/main.py
@@ -13,7 +13,7 @@ from bin.getCandidateKmers import _getAllCandidateKmers
 from bin.removeOutgroupPrimers import _removeOutgroupPrimers
 
 # global constants
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 __author__ = "Joseph S. Wirth"
 
 # functions
